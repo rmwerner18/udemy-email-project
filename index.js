d699e1361e67e65d140049ac035b0ccd7fe2a789
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-27 22:49:53 -0500

    get google keys

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth-20').Strategy
+const keys = require('./config/keys')
 
 const app = express();
 
-passport.use(new googleStrategy())
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+}, accessToken => {
+    console.log(accessToken)
+}))
 
 
 // app.get('/', (req, res) => {
