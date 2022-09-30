82eee9a525915d5759223e8e7aadec1c4691f497
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-30 18:56:48 -0500

    finish passport callback

--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,23 @@
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(new googleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken)
-        console.log('refreshToken', refreshToken)
-        console.log('profile', profile)
-        console.log('done', done)
+        User.findOne({ googleID: profile.id }).then(existingUser => {
+            if (existingUser) {
+                done(null, existingUser)
+            } else {
+                new User({ googleID: profile.id })
+                    .save() 
+                    .then(user => done(null, user))
+            }
+        })
     }
 ))
