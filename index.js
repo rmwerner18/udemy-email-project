cd38cbcae153a753154d2e3ffde60ed4b1a57a6c
humorous-storage-7521 <rmwerner@umich.edu>
2022-09-30 10:38:21 -0500

    refactor code

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
-const express = require('express');
-const passport = require('passport')
-const googleStrategy = require('passport-google-oauth20').Strategy
-const keys = require('./config/keys')
+const express = require('express')
+require('./services/passport')
 
-const app = express();
+const app = express()
 
-passport.use(new googleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-    }, (accessToken, refreshToken, profile, done) => {
-        console.log('accessToken', accessToken)
-        console.log('refreshToken', refreshToken)
-        console.log('profile', profile)
-        console.log('done', done)
-    }
-))
-
-app.get(
-    '/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-)
-
-app.get('/auth/google/callback', passport.authenticate('google'))
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 
