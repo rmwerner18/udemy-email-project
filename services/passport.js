952e4e14296b61997994b8e3a0e3599f5136b827
humorous-storage-7521 <rmwerner@umich.edu>
2022-10-05 15:36:24 -0500

    allow proxy in google strategy

--- services/passport.js
+++ services/passport.js
@@ -18,6 +18,7 @@ passport.use(new googleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
+    proxy: true,
     }, (accessToken, refreshToken, profile, done) => {
         User.findOne({ googleID: profile.id }).then(existingUser => {
             if (existingUser) {
