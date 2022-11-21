0978e6af0642dcf2ffd9b719caa135826889d90d
humorous-storage-7521 <rmwerner@umich.edu>
2022-11-20 22:28:05 -0600

    add body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 require('./models/User')
@@ -10,11 +11,11 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json())
 app.use(cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
 }))
-
 app.use(passport.initialize())
 app.use(passport.session())
 
