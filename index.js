2b2fda4e9e7d74a31641f40f8e8af58b94b988a0
humorous-storage-7521 <rmwerner@umich.edu>
2023-01-05 21:23:05 -0600

    make survey model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
 require('./models/User')
+require('./models/Survey')
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
@@ -21,6 +22,7 @@ app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
+require('./routes/surveyRoutes')(app)
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const requireLogin = require('../middlewares/requireLogin')
+const requireLogin = require('../middlewares/requireCredit')
+const requireCredit = require('../middlewares/requireCredit')
+
+const Survey = mongoose.model('surveys')
+
+module.exports = app => {
+ app.post('api/surveys', requireLogin, requireCredit, (req, res) => {
+  const { title, subject, body, recipients } = req.body
+
+  const survey = new Survey({
+   title,
+   subject, 
+   body,
+   recipients: recipients.split(',').map(email => ({ email })),
+   _user: req.user.id,
+   dateSent: Date.now()
+  })
+ })
+}
+
