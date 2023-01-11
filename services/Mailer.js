ae826dd56a5491b13c96510a18fd8664a3c6c039
humorous-storage-7521 <rmwerner@umich.edu>
2023-01-10 23:22:06 -0600

    debug emails send

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,54 +1,55 @@
-const sendgrid = require('sendgrid')
-const helper = sendgrid.mail
-const keys = require('../config/keys')
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
- constructor( { subject, recipients }, content) {
-  super()
+  constructor({ subject, recipients }, content) {
+    super();
 
-  this.sgApi = sendgrid(keys.sendGridKey)
-  this.from_email = new helper.Email('ryanwerner50@gmail.com')
-  this.subject = subject
-  this.body = new helper.Content('text/html', content)
-  this.recipients = this.formatAddresses(recipients)
+    this.sgApi = sendgrid(keys.sendGridKey);
+    this.from_email = new helper.Email('ryanwerner50@gmail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
 
-  this.addContent(this.body)
-  this.addClickTracking()
-  this.addRecipients()
- }
- 
- formatAddresses(recipients) {
-  return recipients.map(({ email }) => {
-   return new helper.Email(email)
-  })
- }
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
 
- addClickTracking() {
-  const trackingSettings = new helper.TrackingSettings()
-  const clickTracking = new helper.ClickTracking(true, true)
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new helper.Email(email);
+    });
+  }
 
-  trackingSettings.setClickTracking(clickTracking)
-  this.addTrackingSettings(TrackingSettings)
- }
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
 
- addRecipients() {
-  const personalize = new helper.Personalization()
-  this.recipients.forEach(recipient => {
-   personalize.addTo(recipient)
-  })
-  this.addPersonalization(personalize)
- }
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
 
- async send() {
-  const request = this.sgApi.emptyRequest({
-   method: 'POST',
-   path: '/v3/mail/send',
-   body: this.toJSON()
-  })
+  addRecipients() {
+    const personalize = new helper.Personalization();
 
-  const response = this.sgApi.API(request)
-  return response
- }
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+
+  async send() {
+    const request = this.sgApi.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: this.toJSON()
+    });
+
+    const response = this.sgApi.API(request);
+    return response;
+  }
 }
 
-module.exports = Mailer
+module.exports = Mailer;
